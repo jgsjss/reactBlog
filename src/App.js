214d2e7a29a46d95d9408596e08c3d65198528c2
jk123 <jgsjss9854@naver.com>
2022-11-27 21:39:46 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+/* eslint-disabled */
+
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+  //즉시 렌더링이 필요할 때 useState를 사용해서 바인딩한다.
+  //state는 등호로 변경할 수 없다.
+  let [a, b] = useState(['리눅스 실전정복','React 학습','겨울 코트 추천']);
+  let [logo, setLogo] = useState('ReactBlog');
+  let [like, sumLike] = useState([0,0,0]); 
+  let [modal, setModal] = useState(false);
+
+  // map의 기능
+  // 1. array 자료 갯수만큼 함수안의 코드를 실행해줌
+  // 2. 함수의 파라미터는 array 안에 있던 자료이다
+  // 3. return에 뭐 적으면 array로 담아준다.
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='black-nav'>
+        <h4 style={{color : 'red', fontSize : '20px'}}>{logo}</h4>
+      </div>
+      {/* <button onClick={()=>{
+        let copyTitle = [...a];
+        copyTitle[1] = 'SQLD 완전정복';
+        b(copyTitle);
+      }}>제목변경버튼</button> */}
+      {/* <button onClick={function(){
+          let copyTitle = [...a];
+          copyTitle.sort();
+          b(copyTitle);
+      }}>글 정렬</button>
+      <div className='list'>
+        <h4>{a[0]} <span onClick={()=>sumLike(like+1)}>👍</span>{like}</h4>
+        <p>발행일</p>
+      </div>
+      <div className='list'>
+        <h4>{a[1]}</h4>
+        <p>발행일</p>
+      </div>
+      <div className='list'>
+        <h4 onClick={()=>{setModal(!modal)}}>{a[2]}</h4>
+        <p>발행일</p>
+      </div> */}
+
+      {
+        a.map(function(e, i){
+          return (
+          <div className='list' key={i}>
+          <h4 onClick={()=>{setModal(!modal)}}>{a[i]}
+          <span onClick={()=>{
+              let copyLike = [...like];
+              copyLike[i] = 1;
+              sumLike(copyLike);}}>👍</span>{like[i]}
+          </h4>
+          <p>발행일</p>
+          </div>)
+            
+          })
+      }
+
+      
+      {
+        modal == true ? <Modal color={'skyblue'} change={b} title={a} /> : null
+      }
+      
+
+      </div>
   );
 }
 
-export default App;
+
+
+function Modal(props){
+  return (
+      <div className="modal" style={{background : props.color}}>
+        <h4>{props.title[0]}</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+        <button onClick={()=>{
+         let copyTitle = [...props.title];
+          copyTitle[0] = 'Vue.js 특강!';
+          props.change(copyTitle);
+        }}>수정</button>
+      </div>
+    )
+  }
+
+
+  export default App;
